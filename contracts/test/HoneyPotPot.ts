8ecfdbe3fa2a7a9833106a4c19ca3fea49b48f57
Soolking-cyber <shanazar15071994@gmail.com>
2025-10-28 16:16:46 +0700

    h

--- contracts/test/HoneyPotPot.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-describe("HoneyPotPot", () => {
-  it.skip("deploys and seeds the initial pot", async () => {
-    // TODO: wire deployment fixture once swap adapter + thirdweb mocks are available.
-    expect(true).to.equal(false);
-  });
-});
-
